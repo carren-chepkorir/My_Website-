90d440d736ee6fdf271c2e6dbc87d2e6b8b142ee
carren <chepkorircarren9@gmail.com>
2023-08-07 12:01:04 +0300

    initial techwrite changes

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -8,36 +8,44 @@ import {
   FaPhone,
   FaWhatsapp,
 } from "react-icons/fa";
+import About from "../Pages/About";
 
 const HomePage = () => {
   return (
-    <div className="h-[100vh] scroll-smooth ">
+    <div className="h-[100vh] ">
       <Navbar />
-      <div className="flex flex-wrap justify-center pt-[100px] ">
-        <div>
-          <h1 className=" text-6xl span text-[#5b7c99] leading-loose text-bold ">
-            Hi,I am Carren Chepkorir
-          </h1>
-          <p className="inline-flex text-4xl font-bold text-orange-600 h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
-            Passionate Software Developer
-          </p>
+
+      <div className="flex flex-wrap  flex-col ">
+        <div className="flex flex-wrap flex-row justify-center items-center">
+          <div className="flex flex-col ">
+            {" "}
+            <h1 className=" text-6xl span text-[#5b7c99] leading-loose text-bold ">
+              Hi,I am Carren Chepkorir
+            </h1>
+            <p className="inline-flex text-4xl font-bold text-orange-600 h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
+              Passionate Software Developer
+            </p>
+          </div>
         </div>
-        <div className="pl-[500px] pr-[500px] leading-loose pt-6">
+        <div className=" flex flex-wrap leading-loose pt-6 flex-row justify-center items-center">
           <p>
             Looking for a full-stack developer with a track record of success?
             Look no further!With completion of a bachelor's degree in Computer
+            <br></br>
             Science and also various hands-on experience have gained through
             programming projects and bootcamps ,I have the immense ability and
+            <br></br>
             passion to create scalable and intuitive UIs.I have a solid
             experience in analysis,design,development and implementation of
-            various application using JavaScript/React,Typescript and C# .NET.I
-            love the fact that I could make an impact to people via my
-            code,whatever tool i could build to help them solve their daily life
-            sounds super exciting to me.Let's build something together!
+            various <br></br>application using JavaScript/React,Typescript and
+            C# .NET.I love the fact that I could make an impact to people via my
+            code, whatever <br></br>tool i could build to help them solve their
+            daily life sounds super exciting to me.Let's build something
+            together!
           </p>
         </div>
       </div>
-      <div className="flex flex-row space-between space-x-5 flex-wrap mt-5 justify-center ">
+      <div className="flex flex-row space-between space-x-10 flex-wrap mt-5 justify-center  ">
         <div>
           <a href="https://github.com/carren-chepkorir" className="href">
             <FaGithub size={50} />
--- src/Components/Techwrite.jsx
+++ src/Components/Techwrite.jsx
@@ -1,18 +1,93 @@
 import React from "react";
+import Techw1 from "../assets/mvc.jpeg";
+import Techw2 from "../assets/Techwrite.webp";
+import Techw3 from "../assets/Techwrite2.webp";
 
 const Techwrite = () => {
   return (
     <div>
-      <div className="flex flex-wrap flex-row ">
+      <div className="flex flex-wrap flex-row pl-20 pr-20">
         <div className="flex flex-col">
           <h1 className="text-2xl font-bold  ">Blog</h1>
           <p className="flex leading-loose">
             I love documenting my work and teaching other Developers. Here are
             some of my Articles have written on
             <a href="https://dev.to/carrenchepkorir/" className="href">
-              <span className="text-blue-500 ml-2">Dev.io</span>
+              <span className="text-orange-500 ml-2">Dev.io</span>
             </a>
           </p>
+        </div>
+      </div>
+      <div className="flex flex-col pl-20 pr-20 mt-8">
+        <div className="flex flex-row items-center ">
+          <div className="transform  bg-white transition duration-400 hover:scale-125 hover:bg-white">
+            <img className="h-[350px] w-[500px]" src={Techw1} alt="" />
+            <h2 className="text-orange-500 text-2xl font-semibold ">
+              Routing in MVC
+            </h2>
+            <p>
+              In this article ,i will explains routing in MVC. How a route in
+              MVC is executed by the routing engine and how to define a route
+              for a URL
+            </p>
+
+            <div className="flex flex-wrap justify-center items-center">
+              <a
+                href="https://dev.to/carrenchepkorir/routing-in-mvc-56mm"
+                className=""
+              >
+                <button className="py-2 px-3 rounded-lg bg-slate-300 text-center">
+                  READMORE
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="md:ml-5 transform  bg-white transition duration-400 hover:scale-125 hover:bg-white">
+            <img className="h-[350px] w-[500px]" src={Techw2} alt="" />
+            <h2 className="text-orange-500 text-2xl font-semibold">
+              Mastering Technical Writing
+            </h2>
+            <p>
+              This article is a step by step guide for beginners in Technical{" "}
+              <br></br>writing.Mastering Technical writing requires a
+              combination <br></br>of skills,techniques and practices.It entails
+              what is Technical writing,Essential Tips and Technique and the
+              impact on the Industry.
+            </p>
+            <div className="flex flex-wrap justify-center tems-center">
+              <a
+                href="https://dev.to/carrenchepkorir/mastering-technical-writingultimate-guide-for-beginners-40j9"
+                className="href"
+              >
+                <button className="py-2 px-3 rounded-lg bg-slate-300 text-center i">
+                  READMORE
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="md:ml-5 transform  bg-white transition duration-400 hover:scale-125 hover:bg-white">
+            <img className="h-[350px] w-[500px]" src={Techw3} alt="" />
+            <h2 className="text-orange-500 text-2xl font-semibold  ">
+              Importance of staying updated with emerging trends in technical
+              writing.
+            </h2>
+            <p>
+              In this article,we are going to discuss the importance of staying
+              updated as a Technical writer with the Technology trends and
+              impact of the same.
+            </p>
+            <div className="flex flex-wrap justify-center items-center">
+              <a
+                href="https://dev.to/carrenchepkorir/importance-of-staying-updated-with-emerging-trends-in-technical-writing-4a3e"
+                className="href"
+              >
+                {" "}
+                <button className="py-2 px-3 rounded-lg bg-slate-300 text-center">
+                  READMORE
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
